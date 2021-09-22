da355bf98616cc7bd3035d8eef2767e2675fb0f5
troy213 <tritera.erlangga@gmail.com>
2021-09-22 21:44:11 +0700

    server routing and create controller

---
+++ server/controller/product_controller.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql')
+require('dotenv').config()
+
+const db = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+})
+
+const productGet = (req, res) => {
+  if (Object.keys(req.query).length === 0) {
+    const sql = 'SELECT * FROM product'
+    db.query(sql, (err, result) => {
+      if (err) throw err
+      return res.status(200).json({ success: true, data: result })
+    })
+  }
+  const { search } = req.query
+  const sql = 'SELECT * FROM product WHERE name=?'
+  db.query(sql, search, (err, result) => {
+    if (err) throw err
+    return res.status(200).json({ success: true, data: result })
+  })
+}
+
+const productGetId = (req, res) => {
+  const { id } = req.params
+  const sql = 'SELECT * FROM product WHERE id=?'
+  db.query(sql, id, (err, result) => {
+    if (err) throw err
+    return res.status(200).json({ success: true, data: result })
+  })
+}
+
+module.exports = { productGet, productGetId }
--- server/index.js
+++ server/index.js
@@ -3,9 +3,14 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5000
+
 const products = require('./product/Products')
+const userRouter = require('./router/user_router')
+const productRouter = require('./router/product_router')
 
 app.use([cors(), express.json(), express.urlencoded({ extended: false })])
+app.use('/user', userRouter)
+app.use('/product', productRouter)
 
 app.get('/', (req, res) => {
   res.status(200).json({ success: true, data: products })
