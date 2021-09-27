d09636cc790120577f9b9f16f404136437c0a6c3
troy213 <tritera.erlangga@gmail.com>
2021-09-27 22:40:10 +0700

    cart fetch hotfix

--- client/src/hooks/UseFetch.js
+++ client/src/hooks/UseFetch.js
@@ -6,44 +6,46 @@ export const useFetch = (url, headers) => {
   const [isError, setIsError] = useState(false)
 
   useEffect(() => {
-    if (headers != null) {
-      fetch(url, {
-        headers: headers,
-      })
-        .then((res) => {
-          if (res.status >= 200 && res.status <= 299) {
-            return res.json()
-          } else {
+    if (data.length === 0) {
+      if (headers != null) {
+        fetch(url, {
+          headers: headers,
+        })
+          .then((res) => {
+            if (res.status >= 200 && res.status <= 299) {
+              return res.json()
+            } else {
+              setIsLoading(false)
+              setIsError(true)
+            }
+          })
+          .then((value) => {
+            setData(value.data)
             setIsLoading(false)
-            setIsError(true)
-          }
-        })
-        .then((value) => {
-          setData(value.data)
-          setIsLoading(false)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    } else {
-      fetch(url)
-        .then((res) => {
-          if (res.status >= 200 && res.status <= 299) {
-            return res.json()
-          } else {
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      } else {
+        fetch(url)
+          .then((res) => {
+            if (res.status >= 200 && res.status <= 299) {
+              return res.json()
+            } else {
+              setIsLoading(false)
+              setIsError(true)
+            }
+          })
+          .then((value) => {
+            setData(value.data)
             setIsLoading(false)
-            setIsError(true)
-          }
-        })
-        .then((value) => {
-          setData(value.data)
-          setIsLoading(false)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }
     }
-  }, [headers, url])
+  }, [headers, url, data.length])
 
   return { data, isLoading, isError }
 }
